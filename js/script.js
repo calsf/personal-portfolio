77f0f6b4e6ecd889e25f7513a138f8a6363341fe
Calvin C <lefrogjones@gmail.com>
2018-08-09 18:21:52 -0400

    Slideshow slides fade in, can change pages now

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 let currentSlide = 1;
+let currentPage = document.getElementById("about-nav");
+changePage("about-container");
+changeNav("about-nav");
 
 function nextSlide(n) 
 {
@@ -27,8 +30,32 @@ function showSlide(n)
 	}
 
 	activeDot[0].classList.remove("active-dot");
-	activeSlide[0].className = activeSlide[0].className.replace("active-slide", "hide-slide");
+	activeSlide[0].className = activeSlide[0].className.replace("active-slide", "hide");
 	slides[currentSlide-1].classList.add("active-slide");
 	dots[currentSlide-1].classList.add("active-dot");
 
+}
+
+function changePage(page)
+{
+	let arr = [document.getElementById("about-container"), document.getElementById("slideshow-container"), document.getElementById("contact-container")]
+	for(let i = 0; i < arr.length; i++)
+	{
+		arr[i].classList.add("hide");
+		arr[i].classList.remove("show");
+	}
+	let pageToShow = document.getElementById(page);
+	pageToShow.classList.remove("hide");
+	pageToShow.classList.add("show");
+}
+
+function changeNav(page)
+{
+	let arr = [document.getElementById("about-nav"), document.getElementById("projects-nav"), document.getElementById("contact-nav")]
+	for(let i = 0; i < arr.length; i++)
+	{
+		arr[i].classList.remove("active-page");
+	}
+	let currentNav = document.getElementById(page);
+	currentNav.classList.add("active-page");
 }
